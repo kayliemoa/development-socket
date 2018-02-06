cf5ed7f26559cb292e11ead08c7f43daf120db4a
Kaylie Son <kayliemoa@gmail.com>
2018-02-06 12:26:31 -0800

    1
    aa

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get("/", (req, resp) => {
+    resp.end("Hi Welcome to my Heroku.bye");
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log("Port is runnig");
+})
+
+
+
+/* Server*/
+
+const server = require("http").Server();
+const port = 10001;
+
+var io = require("socket.io")(server);
+
+var usernames = [];
+var msgs = [];
+
+io.on("connection", function(socket){
+    
+   console.log("user is connected"); 
+    
+    socket.on("username", function(data){
+        console.log("user is giving username:"+data);
+        usernames.push(data);
+        
+        io.emit("usersjoined", usernames);
+        
+    })
+    
+    socket.on("sendChat", function(data){
+        console.log("user sent a msg for chat");
+        msgs.push(data);
+        
+        io.emit("msgsent", msgs);
+    });
+    
+    socket.on("disconnect", function(){
+        console.log("user has disconnected");
+    })
+    
+});
+
+server.listen(port, (err)=>{
+    if(err){
+        console.log("Error:" +err);
+        return false;
+        
+    }
+    console.log("socket port is running");
+});
+
+
+
+
+
